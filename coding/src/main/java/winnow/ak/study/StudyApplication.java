f7de339905465c7f3bd3d9fd719c506746fd2493
winyu <lijingyu@iquanwai.com>
2021-05-16 11:02:21 +0800

    add coding spring boot

--- coding/src/main/java/winnow/ak/study/StudyApplication.java
+++ coding/src/main/java/winnow/ak/study/StudyApplication.java
@@ -1,10 +1,16 @@
 package winnow.ak.study;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @Author: Winyu
  * @Date: 2021/5/15
  */
+@SpringBootApplication
 public class StudyApplication {
+
     public static void main(String[] args) {
+        SpringApplication.run(StudyApplication.class,args);
     }
 }
